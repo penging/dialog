dc4634a26f39189b20becbfcce1cedb9615ef366
Denis <swainet@126.com>
2013-03-11 15:34:11 +0800

    first ci

---
+++ src/js/dialog.js
@@ -0,0 +1,10 @@
+define(function (require, exports, module) {
+  var $ = require('$');
+  var Widget = require('widget');
+  var Detect = require('detect');
+
+  var Dialog = Widget.extend({
+
+  });
+  return Dialog;
+});
